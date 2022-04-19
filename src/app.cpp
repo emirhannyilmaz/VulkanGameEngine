082a4b075819738b68f71fbbe260f435e1b0a626
Emirhan Yılmaz <emirhanra@gmail.com>
2022-04-19 17:28:52 +0300

    Rendering multiple entities done

--- src/app.cpp
+++ src/app.cpp
@@ -12,7 +12,9 @@ void App::run() {
     Mesh* mesh = new Mesh(objModelData.vertices, objModelData.indices, renderer);
     Texture* texture = new Texture("res/textures/rocket.png", 1.0f, 10.0f, renderer);
     Entity* entity = new Entity(mesh, texture, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.1f, 0.1f, 0.1f), renderer);
-    Entity* entity2 = new Entity(mesh, texture, glm::vec3(-50.0f, 10.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.1f, 0.1f, 0.1f), renderer);
+    Mesh* mesh2 = new Mesh(objModelData.vertices, objModelData.indices, renderer);
+    Texture* texture2 = new Texture("res/textures/rocket.png", 1.0f, 10.0f, renderer);
+    Entity* entity2 = new Entity(mesh2, texture2, glm::vec3(-50.0f, 10.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.1f, 0.1f, 0.1f), renderer);
 
     std::vector<Entity*> entities;
     entities.push_back(entity);
