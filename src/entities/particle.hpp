1bb0e8738dc7c7bbf02daeb89d9790b32ffe4c76
Emirhan Yılmaz <emirhanra@gmail.com>
2022-08-20 23:09:33 +0300

    Particle class done

--- src/entities/particle.hpp
+++ src/entities/particle.hpp
@@ -2,19 +2,39 @@
 #define particle_hpp
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "../rendering/renderer/renderer.hpp"
+#include "../rendering/renderer/buffer.hpp"
+#include "../rendering/renderer/descriptor_pool.hpp"
+#include "../rendering/renderer/descriptor_set_layout.hpp"
+#include "../rendering/renderer/descriptor_sets.hpp"
 #include "../rendering/renderer/common.hpp"
+
+struct ParticleVertexUniformBufferObject {
+    alignas(16) glm::mat4 modelViewMatrix;
+};
 
 class Particle {
 public:
+    static DescriptorSetLayout* descriptorSetLayout;
+    DescriptorSets* descriptorSets;
     glm::vec3 position;
-    glm::vec3 rotation;
+    float rotation;
     glm::vec3 scale;
     glm::vec3 velocity;
     float gravityMultiplier;
     float lifeLength;
     float elapsedTime = 0.0f;
-    Particle(glm::vec3 position, glm::vec3 rotation, glm::vec3 scale, glm::vec3 velocity, float gravityMultiplier, float lifeLength);
+    Particle(glm::vec3 position, float rotation, glm::vec3 scale, glm::vec3 velocity, float gravityMultiplier, float lifeLength, Renderer* renderer);
+    ~Particle();
     bool update(float deltaTime);
+    void updateDescriptorSetResources(glm::mat4 viewMatrix);
+    static void CreateDesriptorSetLayout(VkDevice& device);
+    static void DeleteDesriptorSetLayout();
+private:
+    DescriptorPool* descriptorPool;
+    Renderer* renderer;
+    std::vector<Buffer*> vertexUniformBuffers;
 };
 
 #endif
